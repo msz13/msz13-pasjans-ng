25a10699b7c27cff5e02a528e5779424d2cce2d7
msz13 <mat.szczecinski@gmail.com>
2021-11-12 14:30:39 +0100

    initial commit

---
+++ apps/msz-pasjans/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'msz-pasjans-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'msz-pasjans';
+}
